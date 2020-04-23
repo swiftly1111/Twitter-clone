304fcb0861b1e8038b98e73cc3b294a5ea10bcb6
swiftly1111 <shinmaru518@gmail.com>
2020-04-23 21:54:28 +0900

    #1 通知画面の追加、メッセージ送付ボタンの追加、画面遷移を追加

--- js/messages.js
+++ js/messages.js
@@ -3,3 +3,8 @@ $(function(){
 		window.location.href = "./search-twitter.html";
 	});
 });
+$(function(){
+	$("#bell_icon").on("click", function() {
+		window.location.href = "./notification.html";
+	});
+});
--- js/search-twitter.js
+++ js/search-twitter.js
@@ -1,4 +1,9 @@
 $(function(){
+	$("#bell_icon").on("click", function() {
+		window.location.href = "./notification.html";
+	});
+});
+$(function(){
 	$("#mail_icon").on("click", function() {
 		window.location.href = "./messages.html";
 	});
