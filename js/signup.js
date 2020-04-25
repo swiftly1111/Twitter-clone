6a6c942897d0a2ff39f6b321641827c4610635ef
takeru <frey.knight15@gmail.com>
2020-04-25 11:21:14 +0900

    #1 ユーザー検索画面を作成

---
+++ js/signup.js
@@ -0,0 +1,12 @@
+
+$(function() {
+    $("#registerBtn").on("click", function(){
+        window.location.href = "./login.html";
+    });
+
+    $("#cancelBtn").on("click", function(){
+        window.location.href = "./login.html";
+    });
+})
+
+
